51d50bd27a8e9b6f4381d85ba20168a0d6a87d34
Admin <Admin@TEBELEVDISK>
2019-05-14 15:14:39 +0200

    Har gjort klass till fireball för att det ska kollidera med Spacerocket.

---
+++ Space rocket/Space rocket/Fireball.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_rocket
+{
+    class Fireball
+    {
+        Vector2 position;
+        Texture2D texture;
+        float speed = 5;
+
+        public Fireball(Texture2D Texture, Vector2 position)
+        {
+            this.texture = Texture;
+            this.position = position;
+        }
+        public void Update()
+        {
+            position.Y += speed;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Rectangle(position.ToPoint(), new Point(15, 15)), Color.White);
+
+        }
+    }
+}
---
+++ Space rocket/Space rocket/Position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Space_rocket
+{
+    internal class Position
+    {
+        internal static int Topoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int intTopoint()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
